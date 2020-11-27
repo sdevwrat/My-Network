92cd55debbf722b69069bc015af5fa15cc5f8215
Devwrat Sahu <sdevwrat@gmail.com>
2020-11-27 12:04:57 +0530

    deploy

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import AppWithRouter from './components/AppWithRouter';
+
+const App = () => <AppWithRouter />;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppWithRouter from './components/AppWithRouter';
+import App from './App';
 import registerServiceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import jwtDecode from 'jwt-decode';
@@ -26,7 +26,7 @@ if(localStorage.jwtToken) {
 
 ReactDOM.render(
   <Provider store={store}>
-    <AppWithRouter />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
